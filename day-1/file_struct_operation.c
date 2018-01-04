f7d36f5ccc53439860c62922f5716aea784b2e40
DoubleGithub <403219317@qq.com>
2018-01-04 21:56:50 +0800

    test

--- day-1/file_struct_operation.c
+++ day-1/file_struct_operation.c
@@ -16,9 +16,29 @@ void initial_struct(student *pstu,int count)
         scanf("%d%s%f",&(pstu+i)->num,(pstu+i)->name,&(pstu+i)->score);
     }
 }
+void output_struct(student *pstu,int count)
+{
+    int i;
+    for(i=0;i<count;i++)
+    {
+        printf("%d\t%s\t%f\n",(pstu+i)->num,(pstu+i)->name,(pstu+i)->score);
+    }
+}
+void file_write(student *pstu,int count,char *path)
+{
+    int i;
+
+}
 int main(int argc,char *argv[])
 {
     student stu[3];
-    printf("%d\n",sizeof(student));
+    FILE *fpr=NULL;
+    FILE *fpw=NULL;
+    FILE *fprb=NULL;
+    FILE *fpwb=NULL;
+    
+    initial_struct(stu,3);
+    output_struct(stu,3);
+    //printf("%d\n",sizeof(student));
     return 0;
 }
